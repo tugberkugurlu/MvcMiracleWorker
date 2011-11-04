a9d6bb030aca141fb74255446bfb48bf66145469
Tugberk Ugurlu <info@tugberkugurlu.com>
2011-11-04 13:28:44 +0200

    added client validation support for GreaterThanAttributeTugberkUgPackage sample

---
+++ packages/TugberkUg.MVC.JQuery.0.2/Content/Scripts/jquery.tugberkug.validation.greaterthandate.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="jquery-1.7-vsdoc.js" />
+/// <reference path="jquery.validate-vsdoc.js" />
+/// <reference path="jquery.validate.unobtrusive.js" />
+
+jQuery.validator.addMethod("greaterthandate", function (value, element, param) {
+
+    return Date.parse(value) > Date.parse($(param).val());
+});
+
+jQuery.validator.unobtrusive.adapters.add("greaterthandate", ["otherpropertyname"], function (options) {
+    options.rules["greaterthandate"] = "#" + options.params.otherpropertyname
+    options.messages["greaterthandate"] = options.message;
+});

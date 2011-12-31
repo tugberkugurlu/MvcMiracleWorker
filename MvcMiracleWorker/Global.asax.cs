fa0addb02281b202c8fd2112d6916db0b114c67c
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2011-12-31 17:56:28 +0200

    added CSRazorViewEngine on CSViewEngine branch

--- MvcMiracleWorker/Global.asax.cs
+++ MvcMiracleWorker/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using MvcMiracleWorker.Application;
 
 namespace MvcMiracleWorker
 {
@@ -29,12 +30,18 @@ namespace MvcMiracleWorker
 
         }
 
+        public static void RegisterViewEngines() {
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new CSRazorViewEngine());
+        }
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
 
             RegisterGlobalFilters(GlobalFilters.Filters);
             RegisterRoutes(RouteTable.Routes);
+            RegisterViewEngines();
         }
     }
 }

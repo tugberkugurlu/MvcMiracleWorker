762d77da14fb61ec7b8d44bbae451bd32717b479
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-01-21 18:10:18 +0200

    added HTML5HistoryAPI sample and fixed nupkg file issue

---
+++ MvcMiracleWorker/Areas/HTML5HistoryAPI/Controllers/SampleController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TugberkUg.MVC.Helpers;
+
+namespace MvcMiracleWorker.Areas.HTML5HistoryAPI.Controllers {
+
+    public class SampleController : Controller {
+
+        public ActionResult Index() {
+
+            if (Request.IsAjaxRequest()) {
+                return Json(new { result = string.Empty }, JsonRequestBehavior.AllowGet);
+            }
+
+            return View();
+        }
+
+        public ActionResult City(string q) {
+
+            System.Threading.Thread.Sleep(2000);
+
+            ViewBag.Content = string.Format("You have requested for city {0}", q);
+
+            if (Request.IsAjaxRequest()) {
+
+                return Json(new { result = this.RenderPartialViewToString("_CityInfo") }, JsonRequestBehavior.AllowGet);
+            }
+
+            return View("index");
+        }
+    }
+}
---
+++ MvcMiracleWorker/Areas/HTML5HistoryAPI/HTML5HistoryAPIAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace MvcMiracleWorker.Areas.HTML5HistoryAPI
+{
+    public class HTML5HistoryAPIAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "HTML5HistoryAPI";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) {
+
+            context.MapRoute(
+                "HTML5HistoryAPI_default",
+                "HTML5HistoryAPI/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
